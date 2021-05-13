5e86824b597fce647318d0ee1f69bf8513cc1670
santiagoGuastavino <san.guasta@gmail.com>
2021-05-13 14:07:31 -0300

    if description = undefined

--- src/database/dish.js
+++ src/database/dish.js
@@ -3,6 +3,7 @@ let dish = [
         id: 1,
         name: 'Carpaccio fresco',
         description: 'Entrada Carpaccio de salmón con cítricos',
+        // longDescription: '',
         price: 'U$S 65.50',
         link: '/detail/1',
         img: '/img/Carpaccio-de-salmon.jpg'
@@ -11,6 +12,7 @@ let dish = [
         id: 2,
         name: 'Risotto de berenjena',
         description: 'Risotto de berenjena y queso de cabra',
+        longDescription: 'Risotto de berenjena y queso de cabra Risotto de berenjena y queso de cabra Risotto de berenjena y queso de cabra Risotto de berenjena y queso de cabra Risotto de berenjena y queso de cabra Risotto de berenjena y queso de cabra Risotto de berenjena y queso de cabra Risotto de berenjena y queso de cabra Risotto de berenjena y queso de cabra Risotto de berenjena y queso de cabra',
         price: 'U$S 47.00',
         link: '/detail/2',
         img: '/img/Risotto-berenjena-queso-cabra.jpg'
@@ -19,6 +21,7 @@ let dish = [
         id: 3,
         name: 'Mousse de arroz',
         description: 'Mousse de arroz con leche y aroma de azahar',
+        // longDescription: '',
         price: 'U$S 37.50',
         link: '/detail/3',
         img: '/img/Mousse-de-arroz-con-leche.jpg'
@@ -27,6 +30,7 @@ let dish = [
         id: 4,
         name: 'Espárragos blancos',
         description: 'Espárragos blancos con vinagreta de verduras y jamón ibérico',
+        longDescription: 'Espárragos blancos con vinagreta de verduras y jamón ibérico Espárragos blancos con vinagreta de verduras y jamón ibérico Espárragos blancos con vinagreta de verduras y jamón ibérico Espárragos blancos con vinagreta de verduras y jamón ibérico Espárragos blancos con vinagreta de verduras y jamón ibérico Espárragos blancos con vinagreta de verduras y jamón ibérico Espárragos blancos con vinagreta de verduras y jamón ibérico Espárragos blancos con vinagreta de verduras y jamón ibérico',
         price: 'U$S 37.50',
         link: '/detail/4',
         img: '/img/esparragos.png'
@@ -35,6 +39,7 @@ let dish = [
         id: 5,
         name: 'Pan con dulce de leche',
         description: 'Pan gourmet artesanal tostado y dulce de leche de vaca virgen',
+        // longDescription: '',
         price: 'U$S 24.99',
         link: '/detail/5',
         img: '/img/pan-dulce-leche.jpg'
@@ -43,6 +48,7 @@ let dish = [
         id: 6,
         name: 'Achuras pampeanas',
         description: 'Chinchulines, mollejas y otros bocados que logran un plato regional típico',
+        longDescription: 'Chinchulines, mollejas y otros bocados que logran un plato regional típico Chinchulines, mollejas y otros bocados que logran un plato regional típico Chinchulines, mollejas y otros bocados que logran un plato regional típico Chinchulines, mollejas y otros bocados que logran un plato regional típico Chinchulines, mollejas y otros bocados que logran un plato regional típico Chinchulines, mollejas y otros bocados que logran un plato regional típico Chinchulines, mollejas y otros bocados que logran un plato regional típico ',
         price: 'U$S 72.00',
         link: '/detail/6',
         img: '/img/achuras.jpg'
